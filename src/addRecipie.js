b5c2d20c3dc73f8b2e1bf312d2f7fa883dcd7fb3
Naomi Yonekura <yonekuranm@msoe.edu>
2018-05-17 10:33:19 -0500

    Fixed dynamic input fields with help from Jason

--- src/addRecipie.js
+++ src/addRecipie.js
@@ -30,20 +30,22 @@ export default class AddRecipie extends React.Component {
         this.setState({ steps: event.value });
     }
 
-    changeIngredient(index, event) {
+    changeIngredient(index, newValue) {
         const newIngredients = this.state.ingredients.map((ingredient, newIndex) => {
             if (index !== newIndex) return ingredient;
             // Make the new input the value of the ingridient at that index
-            return { ingredient, name: event.value };
+            ingredient.name = newValue;
+            return ingredient;
         });
         this.setState({ ingredients: newIngredients });
     }
 
-    changeAmount(index, event) {
+    changeAmount(index, newValue) {
         const newAmount = this.state.ingredients.map((ingredient, newIndex) => {
             if (index !== newIndex) return ingredient;
             // Make the new amount the value of the ingridient at that index
-            return { ingredient, amount: event.value };
+            ingredient.amount = newValue;
+            return ingredient;
         });
         this.setState({ ingredients: newAmount });
     }
@@ -51,6 +53,7 @@ export default class AddRecipie extends React.Component {
     handleRemove(index) {
         this.setState({
             // Remove the value if it's index is the same as the pass in index
+            // ingredients: this.state.ingredients.filter((ingredient, newIndex) => index !== newIndex)
             ingredients: this.state.ingredients.filter((ingredient, newIndex) => index !== newIndex)
         });
     }
@@ -76,22 +79,21 @@ export default class AddRecipie extends React.Component {
 
 
                 {this.state.ingredients.map((ingredient, index) => (
-                    <div>
-                        <TextField key={`I${index}`}
+                    <div key={index}>
+                        <TextField
                             floatingLabelText="Ingredient"
                             value={ingredient.name}
-                            onChange={() => this.changeIngredient(index, this)} />
+                            onChange={(event, newValue) => this.changeIngredient(index, newValue)} />
 
-                        <TextField key={`A${index}`}
+                        <TextField
                             floatingLabelText="Amount"
                             value={ingredient.amount}
-                            onChange={() => this.changeAmount(index, this)} />
+                            onChange={(event, newValue) => this.changeAmount(index, newValue)} />
 
 
                         <FloatingActionButton secondary={true} onClick={() => this.handleRemove(index)}>
                             <Remove />
                         </FloatingActionButton>
-                        {/* <RaisedButton secondary={true} onClick={() => this.handleRemove(index)}>-</RaisedButton> */}
 
                     </div>
                 ))}
